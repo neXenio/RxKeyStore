c93f624415c9434f924f087e60d62f537940d666
Marvin Mirtschin <marvin.mirtschin@nexenio.com>
2021-09-22 09:04:48 +0200

    adds tests to test new methods and show behavior of different key stores

--- rxkeystore/src/androidTest/java/com/nexenio/rxkeystore/provider/cipher/asymmetric/rsa/RsaCipherProviderTest.java
+++ rxkeystore/src/androidTest/java/com/nexenio/rxkeystore/provider/cipher/asymmetric/rsa/RsaCipherProviderTest.java
@@ -1,5 +1,8 @@
 package com.nexenio.rxkeystore.provider.cipher.asymmetric.rsa;
 
+import android.security.keystore.KeyProperties;
+import android.util.Base64;
+
 import com.nexenio.rxkeystore.RxKeyStore;
 import com.nexenio.rxkeystore.provider.cipher.RxEncryptionException;
 import com.nexenio.rxkeystore.provider.cipher.asymmetric.BaseAsymmetricCipherProviderTest;
@@ -18,6 +21,9 @@ import androidx.annotation.NonNull;
 
 public class RsaCipherProviderTest extends BaseAsymmetricCipherProviderTest {
 
+    protected RxAsymmetricCipherProvider defaultProvider;
+    protected RxAsymmetricCipherProvider bouncyCastleProvider;
+
     @BeforeClass
     public static void setUpBeforeClass() {
         setupSecurityProviders();
@@ -33,6 +39,8 @@ public class RsaCipherProviderTest extends BaseAsymmetricCipherProviderTest {
     @Override
     public void setUpBeforeEachTest() {
         super.setUpBeforeEachTest();
+        bouncyCastleProvider = asymmetricCryptoProvider;
+        defaultProvider = createAsymmetricCryptoProvider(new RxKeyStore());
     }
 
     @Override
@@ -51,11 +59,73 @@ public class RsaCipherProviderTest extends BaseAsymmetricCipherProviderTest {
     public void encrypt_invalidDataLength_emitsError() {
         byte[] unencryptedBytes = LOREM_IPSUM_LONG.getBytes(StandardCharsets.UTF_8);
 
-        asymmetricCryptoProvider.generateKeyPair(ALIAS_NEW, context)
-                .flatMap(keyPair -> asymmetricCryptoProvider.encrypt(unencryptedBytes, keyPair.getPublic())
-                        .flatMap(encryptedBytesAndIV -> asymmetricCryptoProvider.decrypt(encryptedBytesAndIV.first, encryptedBytesAndIV.second, keyPair.getPrivate())))
+        bouncyCastleProvider.generateKeyPair(ALIAS_NEW, context)
+                .flatMap(keyPair -> bouncyCastleProvider.encrypt(unencryptedBytes, keyPair.getPublic())
+                        .flatMap(encryptedBytesAndIV -> bouncyCastleProvider.decrypt(encryptedBytesAndIV.first, encryptedBytesAndIV.second, keyPair.getPrivate())))
                 .test()
                 .assertError(RxEncryptionException.class);
+    }
+
+    @Test
+    public void encrypt_publicKey$wrongPurpose$defaultProvider_emitsNoError() {
+        byte[] unencryptedBytes = Base64.decode(ENCODED_SYMMETRIC_KEY, Base64.DEFAULT);
+
+        defaultProvider.generateKeyPair(ALIAS_NEW, KeyProperties.PURPOSE_DECRYPT, context)
+                .flatMap(keyPair -> defaultProvider.encrypt(unencryptedBytes, keyPair.getPublic()))
+                .test()
+                .assertNoErrors();
+    }
+
+    @Test
+    public void encrypt_privateKey$wrongPurpose$defaultProvider_emitsError() {
+        byte[] unencryptedBytes = Base64.decode(ENCODED_SYMMETRIC_KEY, Base64.DEFAULT);
+
+        defaultProvider.generateKeyPair(ALIAS_NEW, KeyProperties.PURPOSE_DECRYPT, context)
+                .flatMap(keyPair -> defaultProvider.encrypt(unencryptedBytes, keyPair.getPrivate()))
+                .test()
+                .assertError(RxEncryptionException.class);
+    }
+
+    @Test
+    public void encrypt_public$wrongPurpose$bouncyCastleProvider_emitsNoError() {
+        byte[] unencryptedBytes = Base64.decode(ENCODED_SYMMETRIC_KEY, Base64.DEFAULT);
+
+        bouncyCastleProvider.generateKeyPair(ALIAS_NEW, KeyProperties.PURPOSE_DECRYPT, context)
+                .flatMap(keyPair -> bouncyCastleProvider.encrypt(unencryptedBytes, keyPair.getPublic()))
+                .test()
+                .assertNoErrors();
+    }
+
+    @Test
+    public void encrypt_privateKey$wrongPurpose$bouncyCastleProvider_emitsNoError() {
+        byte[] unencryptedBytes = Base64.decode(ENCODED_SYMMETRIC_KEY, Base64.DEFAULT);
+
+        bouncyCastleProvider.generateKeyPair(ALIAS_NEW, KeyProperties.PURPOSE_DECRYPT, context)
+                .flatMap(keyPair -> bouncyCastleProvider.encrypt(unencryptedBytes, keyPair.getPrivate()))
+                .test()
+                .assertNoErrors();
+    }
+
+    @Test
+    public void decrypt_publicKey$wrongPurpose$defaultProvider_emitsNoError() {
+        byte[] unencryptedBytes = Base64.decode(ENCODED_SYMMETRIC_KEY, Base64.DEFAULT);
+
+        defaultProvider.generateKeyPair(ALIAS_NEW, KeyProperties.PURPOSE_DECRYPT, context)
+                .flatMap(keyPair -> defaultProvider.encrypt(unencryptedBytes, keyPair.getPublic())
+                        .flatMap(encryptedBytesAndIV -> defaultProvider.decrypt(encryptedBytesAndIV.first, encryptedBytesAndIV.second, keyPair.getPrivate())))
+                .test()
+                .assertNoErrors();
+    }
+
+    @Test
+    public void decrypt_public$wrongPurpose$bouncyCastleProvider_emitsNoError() {
+        byte[] unencryptedBytes = Base64.decode(ENCODED_SYMMETRIC_KEY, Base64.DEFAULT);
+
+        bouncyCastleProvider.generateKeyPair(ALIAS_NEW, KeyProperties.PURPOSE_DECRYPT, context)
+                .flatMap(keyPair -> bouncyCastleProvider.encrypt(unencryptedBytes, keyPair.getPublic())
+                        .flatMap(encryptedBytesAndIV -> bouncyCastleProvider.decrypt(encryptedBytesAndIV.first, encryptedBytesAndIV.second, keyPair.getPrivate())))
+                .test()
+                .assertNoErrors();
     }
 
 }
